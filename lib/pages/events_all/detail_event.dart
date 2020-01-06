bc46220b882d6aa08c6e3186dd24edf37e272402
ZKRA000 <44751014+ZKRA000@users.noreply.github.com>
2020-01-06 15:02:41 +0700

    baru saja

--- lib/pages/events_all/detail_event.dart
+++ lib/pages/events_all/detail_event.dart
@@ -5,6 +5,12 @@ import 'dart:convert';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:checkin_app/pages/register_event/step_register_one.dart';
 import 'package:http/http.dart' as http;
+import 'package:checkin_app/routes/env.dart';
+import 'package:checkin_app/storage/storage.dart';
+
+
+String tokenType, accessToken;
+Map<String, String> requestHeaders = Map();
 
 class RegisterEvents extends StatefulWidget {
   final int id;
@@ -30,12 +36,24 @@ class _RegisterEvent extends State<RegisterEvents> {
   }
 
   _getAll() async {
-     Map<String, String> head = {'Content-Type':'application/json','Accept':'application/json','Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImE0MWEwNGQzM2NmNzBlNWZjMDk0MGYwMTA5NTQ4ZDFlZjc1NTdjYTNkZjc2ZGE2NTk0ZDg0OTM0ZWIxZWZmOTVmODMwNDUzYWNjMDBiOWQ2In0.eyJhdWQiOiIxIiwianRpIjoiYTQxYTA0ZDMzY2Y3MGU1ZmMwOTQwZjAxMDk1NDhkMWVmNzU1N2NhM2RmNzZkYTY1OTRkODQ5MzRlYjFlZmY5NWY4MzA0NTNhY2MwMGI5ZDYiLCJpYXQiOjE1Nzc5ODE4NzEsIm5iZiI6MTU3Nzk4MTg3MSwiZXhwIjoxNjA5NjA0MjcxLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.T34QK_ocXFDmQCUQhnwMshAvHKqN0a0Jr_13Q8Bv02ez4u19KVH_h6a3Bk90HnpKOJPU3AcOTsanL0H57tGKxV9rFm8nnI1oKm-ZIoASR4MT9XrWd0T2Zj09qBJQY_-pfFRqk1r8G78ic9-_NaMmUhJxua8IdzNs_0DvySm2oIofKEDN4D14IPiSEUwlEBtMHIJXo8eKtiEGMrJbXYD0-P9tJL3vdflZGFTL72OvJdRNjpVgnCQMAuSFVTAtytQDEMnIjH41rNCbw-whyaalQBVIjWIGtwIaAtOX_3b_NcaNF0j8xtRkFMR2bV3p7cLJ77oQmvTVVcguTW15b3TPLje9K0aaYgUVwRpgiGxP3ySwJXfuoarrZ_sFNTMNA0awMlTh5J3iDgfnX33SuLnDOERu3WYd0dpx6fefGbYtbz73J9l7vY2ub5KozWJ3VxpLjIq0UbPor6m_qL7knys-NMDCDfK7uM6ZiI5ioV8W8gN3BPZ2bYYN6rqWtVqKxs5mFQJpRdS11Q-J50Qyf_wTqP3aigUzOGfeqSzmKSmmUfv1CHCQ6rs_RL8UdeHhWmvxDxnMIzdwLqZoBUG5zr1IQn6IXLkp7gwKV4gHRkxOnQuYIJwNPEi1bFm8N9y-e0Kl3ymTBODo-6B9VDGR6WmI0PYlf-yq4eXnghIkEsFnG6w'};
-      String id = widget.id.toString();
-      var data = await http.get('http://localhost:8000/api/event/${id}',headers:head);
+    var storage = new DataStore();
+    var tokenTypeStorage = await storage.getDataString('token_type');
+    var accessTokenStorage = await storage.getDataString('access_token');
 
-      if(data.statusCode == 200){
-        Map rawData = json.decode(data.body);
+    tokenType = tokenTypeStorage;
+    accessToken = accessTokenStorage;
+    requestHeaders['Accept'] = 'application/json';
+    requestHeaders['Authorization'] = '$tokenType $accessToken';
+
+    String id = widget.id.toString();
+
+    final ongoingevent = await http.get(
+        url('api/event/${id}'),
+        headers: requestHeaders,
+      );
+print(ongoingevent.body);
+      if (ongoingevent.statusCode == 200) {
+        Map rawData = json.decode(ongoingevent.body);
         setState((){
            dataEvent = SearchEvent.fromJson(rawData['data']);
            _isLoading = false;
